83fc5670833ae091c799f2c524c5465081c0fcaf
zwz <zouweizhe@guoxintianyu.com>
2023-01-30 13:02:11 +0800

    css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // 部分文件可能一个loader 不能处理完，所以这里的use是一个数组形式,并且使用的顺序是从后往前
+        use: [
+          // 这种方式没有得到一个单独的css文件，并且样式也没有进行压缩，需要实现这部分东西要搭配plugin进行实现
+          {
+            loader: 'style-loader'
+          },
+          {
+            // 单纯的使用css-loader 样式不会生效，因为他只负责解析，但是不会将css加载到html页面上
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
   }
 }
